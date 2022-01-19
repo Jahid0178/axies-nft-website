56f2718500a7f4ebb734ca8c96cfba57d0bd63e9
Zahid <jahidulhaque767@gmail.com>
2022-01-19 09:33:19 +0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Activity from "./Components/Activity/Activity";
+import Community from "./Components/Community/Community";
+import Contact from "./Components/Contact/Contact";
+import Pages from "./Components/Pages/Pages";
 import Navigation from "./Components/Shared/Navigation/Navigation";
 import Explore from "./Pages/Explore/Explore";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div>
+    <>
       <BrowserRouter>
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/explore" element={<Explore />} />
+          <Route path="/activity" element={<Activity />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/pages" element={<Pages />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </>
   );
 }
 
